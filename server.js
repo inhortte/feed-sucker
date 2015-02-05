8affb7eda473f18f22686926b4e43a870b0f6be2
Bobbus Mustelidus <inhortte@gmail.com>
2015-02-05 23:24:02 +0100

    it does the same thurking thing as  before

--- server.js
+++ server.js
@@ -9,72 +9,65 @@ var MongoClient  = require('mongodb').MongoClient;
 
 util.inherits(MongoClient, EventEmitter);
 
-var feedparser = new FeedParser([]);
 var mongoUrl = 'mongodb://localhost:27017/concierge';
 var linkList = [ 'http://www.teatrobreton.org/rss_eventos.asp',
-                 'http://www.teatrobreton.org/rss_noticias.asp' ];
+                 'http://www.teatrobreton.org/rss_noticias.asp',
+                 'http://www.teatrobreton.org/rss_eventos.asp' ];
 
-feedparser.on('error', function(error) {
-  console.log('Feedparser BANG!');
-  process.exit(0);
-});
+var newMeta = function(db, metaData) {
+  if(db) {
+    var siteDoc = db.collection('site');
 
-feedparser.on('meta', function() {
-  var stream = this
-    , meta = this.meta;
-
-  var metaData = {
-    type: meta['#type']
-    , version: meta['#version']
-    , title: meta['title']
-    , description: meta['description']
-    , link: meta['link']
-  };
-  console.log(JSON.stringify(metaData));
-  MongoClient.prototype.emit('newMeta', metaData);
-});
-
-/*
-feedparser.on('readable', function() {
-  var stream = this
-    , meta = this.meta
-    , item;
-
-  var metaData = {
-    type: meta['#type']
-    , version: meta['#version']
-    , title: meta['title']
-    , description: meta['description']
-    , link: meta['link']
-  };
-  var data = {
-
-  };
-  console.log(JSON.stringify(metaData));
-});
-
-var feed = request(teatro_breton);
-feed.on('error', function(error) {
-  console.log('Bang!');
-  process.exit(0);
-});
-
-feed.on('response', function(res) {
-  var stream = this;
-  if(res.statusCode != 200) {
-    return this.emit('error', new Error('Evil status code -> ' + res.statusCode));
+    // If the metadata already exists, do not insert it another time!
+    siteDoc.count({feedLink: metaData['feedLink']}, function(err, count) {
+      assert.equal(err, null);
+      console.log('count is ' + count);
+      if(count == 0) {
+        console.log('about to insert.');
+        siteDoc.insert(metaData, function(err, res) {
+          console.log(metaData._id);
+          assert.equal(err, null);
+          assert.equal(1, res.result.n);
+        });
+      }
+    });
+  } else {
+    return this.emit('error', new Error('The fucking database is not set, vole.'));
   }
-  stream.pipe(feedparser);
-});
-*/
+}
 
-MongoClient.prototype.on('connect', function(db, linkList) {
+MongoClient.prototype.on('newMeta', newMeta);
+
+// MongoClient.prototype.on('connect', function(db, linkList) {
+var connect = function(db, linkList) {
   console.log('connected');
   MongoClient.prototype.db = db;
 
   // This needs to be fully asynchronous eventually. I'm not sure now, but this may have to be a server accepting links.
-  async.eachSeries(linkList, function(link, cb) {
-    var feed = request(link);
+  async.eachSeries(linkList, function(feedLink, cb) {
+    var feedparser = new FeedParser([]);
+    feedparser.on('error', function(error) {
+      console.log('Feedparser BANG! ' + error);
+      process.exit(0);
+    });
+    feedparser.on('meta', function() {
+      var stream = this
+      , meta = this.meta;
+
+      var metaData = {
+        type: meta['#type']
+        , version: meta['#version']
+        , title: meta['title']
+        , description: meta['description']
+        , link: meta['link']
+        , feedLink: feedLink
+      };
+      console.log(JSON.stringify(metaData));
+      // MongoClient.prototype.emit('newMeta', metaData); // no funciona.
+      newMeta(db, metaData);
+    });
+
+    var feed = request(feedLink);
     feed.on('error', function(error) {
       console.log('Bang!');
       process.exit(0);
@@ -93,27 +86,7 @@ MongoClient.prototype.on('connect', function(db, linkList) {
     }
     // MongoClient.prototype.emit('close');
   });
-});
-
-MongoClient.prototype.on('newMeta', function(metaData) {
-  if(MongoClient.prototype.db) {
-    var siteDoc = MongoClient.prototype.db.collection('site');
-
-    // If the metadata already exists, do not insert it another time!
-    siteDoc.count(metaData, function(err, count) {
-      assert.equal(err, null);
-      if(count == 0) {
-        siteDoc.insert(metaData, function(err, res) {
-          console.log(metaData._id);
-          assert.equal(err, null);
-          assert.equal(1, res.result.n);
-        });
-      }
-    });
-  } else {
-    return this.emit('error', new Error('The fucking database is not set, vole.'));
-  }
-});
+};
 
 /*
 MongoClient.prototype.on('newFeed', function(data) {
@@ -135,7 +108,17 @@ MongoClient.prototype.on('close', function() {
   }
 });
 
-MongoClient.connect(mongoUrl, function(error, db) {
-  assert.equal(null, error);
-  MongoClient.prototype.emit('connect', db, linkList);
+async.waterfall([
+  function(cb) {
+    MongoClient.connect(mongoUrl, function(error, db) {
+      assert.equal(null, error);
+//      MongoClient.prototype.emit('connect', db, linkList);
+      cb(null, db);
+    });
+  }, function(db, cb) {
+    connect(db, linkList);
+    cb(null, db);
+  }
+], function(err, db) {
+//  db.close();
 });
